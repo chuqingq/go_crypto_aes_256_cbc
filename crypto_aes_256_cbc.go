70a3084381b73c1de02b6ebf9a912807fd6aa7ea
Chu Qingqing <chuqingq@qq.com>
2024-01-07 22:10:50 +0800

    Add files via upload

---
+++ crypto_aes_256_cbc.go
@@ -0,0 +1,72 @@
+package productinfo
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+func AES256CBCEncrypt(key, plaintext []byte) ([]byte, error) {
+	key = paddingKey(key, 32)
+	plaintext = PKCS5Padding(plaintext, aes.BlockSize)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	// iv大小应该是aes.BlockSize = 16
+	// openssl的iv默认是全0
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, nil
+}
+
+func AES256CBCDecrypt(key []byte, ciphertext []byte) ([]byte, error) {
+	key = paddingKey(key, 32)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	return PKCS5UnPadding(ciphertext), nil
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+func paddingKey(key []byte, size int) []byte {
+	if len(key) >= size {
+		return key[:size]
+	}
+
+	return append(key, bytes.Repeat([]byte{0}, size-len(key))...)
+}
